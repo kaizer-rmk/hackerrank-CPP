4a471708652db36724cc3cba3c085177e492a91c
MUKESH KUMAR <54999444+kaizer-rmk@users.noreply.github.com>
2020-07-05 20:57:37 +0530

    Create inputOutput.cpp
    Input and Output

---
+++ inputOutput.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int num1,num2,num3,num;
+    cin >> num1 >> num2 >> num3;
+    num=num1+num2+num3;
+    cout << num << endl;
+    return 0;
+}
