b4dd72aed30d7735e13069706a697e377f2dd13c
MUKESH KUMAR <54999444+kaizer-rmk@users.noreply.github.com>
2020-07-20 21:46:33 +0530

    Update boxIt.cpp

--- boxIt.cpp
+++ boxIt.cpp
@@ -32,7 +32,7 @@ class Box{
             return((long long)l*b*h);
         }
         friend bool operator < ( Box&A,Box& B){
-            if( (A.l < B.l) || ((A.b < B.b) && (A.l == B.l)) || ((A.h < B.h) && (A.l == B.l)&& (A.b == B.b)) ){
+            if( (A.l < B.l) || ((A.b < B.b) && (A.l == B.l)) || ((A.h < B.h) && (A.l ==  B.l)&& (A.b == B.b)) ){
             return true;
             }
             else{
@@ -46,3 +46,56 @@ class Box{
     }
 
 };
+
+void check2()
+{
+	int n;
+	cin>>n;
+	Box temp;
+	for(int i=0;i<n;i++)
+	{
+		int type;
+		cin>>type;
+		if(type ==1)
+		{
+			cout<<temp<<endl;
+		}
+		if(type == 2)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			temp=NewBox;
+			cout<<temp<<endl;
+		}
+		if(type==3)
+		{
+			int l,b,h;
+			cin>>l>>b>>h;
+			Box NewBox(l,b,h);
+			if(NewBox<temp)
+			{
+				cout<<"Lesser\n";
+			}
+			else
+			{
+				cout<<"Greater\n";
+			}
+		}
+		if(type==4)
+		{
+			cout<<temp.CalculateVolume()<<endl;
+		}
+		if(type==5)
+		{
+			Box NewBox(temp);
+			cout<<NewBox<<endl;
+		}
+
+	}
+}
+
+int main()
+{
+	check2();
+}
